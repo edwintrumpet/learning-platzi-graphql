b01cbba8538e4ab7fd696fa0fa233d47b0745a08
Edwin García <spark.com.co@gmail.com>
2020-04-21 08:53:45 -0500

    Preparando la API para produccion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -9,7 +10,9 @@ const resolvers = require('./lib/resolvers')
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
 const port = process.env.PORT || 3000
+const isDev =  process.env.NODE_ENV !== 'production'
 
 // Definiendo el esquema
 const typeDefs = readFileSync(
@@ -21,7 +24,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers })
 app.use('/api', gqlMiddleware({
   schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
